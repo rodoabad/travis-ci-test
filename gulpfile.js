c026c3b30bb6793341a4715f2eb7b10679fc4506
rodoabad <rodoabad@gmail.com>
2015-01-21 11:57:43 -0600

    Added initial test files.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
+
+gulp.task('lint', function() {
+    return gulp.src('src/hello.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/hello.js')
+        .pipe(rename('hello.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['lint', 'scripts']);
---
+++ src/hello.js
@@ -0,0 +1,6 @@
+(function(window, document, undefined) {
+
+    // Display 'Hello world!'
+    console.log('Hello world!');
+
+})(window, document);
